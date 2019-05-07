6627c5155bd782cbec461301c26e9d120f92b8d1
shivangna <shivangna.kaistha@gmail.com>
2019-05-07 21:04:46 +0000

    renders new static message

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,35 @@ import Message from './Message.jsx';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {loading:true};
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+          id: '1'
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
+          id: '2'
+        }
+      ]
+    }
   }
 
   componentDidMount() {
+    console.log("componentDidMount <App />");
     setTimeout(() => {
-      this.setState(
-     
-        
-        {
-        currentUser: {name: "Bob"},
-        loading:false,
-        messages: [
-          {
-            username: "Bob",
-            content: "Has anyone seen my marbles?",
-            id: 1
-          },
-          {
-            username: "Anonymous",
-            content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-            id: 2
-          }
-        ]
-      });
-    }, 500)
-  
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
   }
-
 
 
 
